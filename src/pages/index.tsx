9d22cd3f244687ba726667b97f5c30ef9f8eb7df
Wendy Hemlock <102826495+PvtWendy@users.noreply.github.com>
2024-09-12 21:08:03 -0400

    Start Game, Place Flags and Reveal Mines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,89 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [field, setField] = useState(
+    Array(81).fill({ isRevealed:false, isMine: false, isFlagged: false })
+  );
+  const [gameRunning, setGameRunning] = useState(false);
+  useEffect(() => {
+    const handleContextmenu = (e: any) => {
+      e.preventDefault();
+    };
+    document.addEventListener("contextmenu", handleContextmenu);
+    return function cleanup() {
+      document.removeEventListener("contextmenu", handleContextmenu);
+    };
+  }, []);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleGameStart = () => {
+    let mineDifficulty = 10
+    const placeMine = Array.from({length: mineDifficulty} , ()=>Math.floor(Math.random() * 1 * 81))
+    const newField = field.map((c, i) => {
+      if(placeMine.includes(i) && mineDifficulty >= 0){
+      mineDifficulty--
+        return { ...c, isMine: true };
+      } else {
+        return c;
+      }
+    });
+    setField(newField);
+    setGameRunning(true);
+  };
+
+  const handleFlag = (index: number) => {
+    const newField = field.map((c, i) => {
+      if (i === index) {
+        if (c.isFlagged) {
+          return { ...c, isFlagged: false };
+        }
+
+        return { ...c, isFlagged: true };
+      } else {
+        return c;
+      }
+    });
+    setField(newField);
+  };
+
+  const handleReveal = (index: number)=>{
+    const newField = field.map((c, i) => {
+      if (i === index) {
+        if (c.isFlagged) {
+          return c;
+        }
+        return { ...c, isRevealed: true };
+      } else {
+        return c;
+      }
+    });
+    setField(newField)
+  }
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      {!gameRunning && (
+        <button
+          onClick={() => handleGameStart()}
+          className="bg-white text-black px-4 py-2 rounded-md "
+        >
+          New Game
+        </button>
+      )}
+      {gameRunning && (
+        <div className="bg-white w-[30rem] h-[30rem] grid grid-cols-9 grid-rows-9 gap-1 p-1 rounded-md ">
+          {field.map((content, index) => (
+            <div
+              key={index}
+              className="bg-slate-900 rounded-md hover:cursor-pointer flex justify-center items-center text-red-500 text-4xl"
+              onAuxClick={() => handleFlag(index)}
+              onClick={()=> handleReveal(index)}
+            >
+              {(content.isMine && content.isRevealed) && "☼"}
+              {content.isFlagged && "⚑"}
+            </div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
     </div>
   );
 }
